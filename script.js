78ed6ffa2756ddab5eb40df656ba481754948e27
Coding2Coding <76713830+Coding2Coding@users.noreply.github.com>
2021-04-18 13:59:12 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function make1stParagraph() {
-    var firstSentence = document.getElementById("1sentence");
-    var secondSentence = document.getElementById("2sentence");
-    var thirdSentence = document.getElementById("3sentence");
-    var fourthSentence = document.getElementById("4sentence");
+    var firstSentence = document.getElementById("1sentence").value;
+    var secondSentence = document.getElementById("2sentence").value;
+    var thirdSentence = document.getElementById("3sentence").value;
+    var fourthSentence = document.getElementById("4sentence").value;
+    firstSentence.push("")
     console.log(firstSentence+" "+secondSentence+" "+thirdSentence+" "+fourthSentence);
     document.getElementById("1paragraph").innerHTML=firstSentence+" "+secondSentence+" "+thirdSentence+" "+fourthSentence;
 }
 
 function make2ndParagraph() {
-    var firstSentenceSecondParagraph = document.getElementById("1sentence2paragraph");
-    var secondSentenceSecondParagraph = document.getElementById("2sentence2paragraph");
-    var thirdSentenceSecondParagraph = document.getElementById("3sentence2paragraph");
-    var fourthSentenceSecondParagraph = document.getElementById("4sentence2paragraph");
+    var firstSentenceSecondParagraph = document.getElementById("1sentence2paragraph").value;
+    var secondSentenceSecondParagraph = document.getElementById("2sentence2paragraph").value;
+    var thirdSentenceSecondParagraph = document.getElementById("3sentence2paragraph").value;
+    var fourthSentenceSecondParagraph = document.getElementById("4sentence2paragraph").value;
     console.log(firstSentenceSecondParagraph+" "+secondSentenceSecondParagraph+" "+thirdSentenceSecondParagraph+" "+fourthSentenceSecondParagraph);
+    document.getElementById("2paragraph").innerHTML=firstSentenceSecondParagraph+" "+secondSentenceSecondParagraph+" "+thirdSentenceSecondParagraph+" "+fourthSentenceSecondParagraph;
 }
