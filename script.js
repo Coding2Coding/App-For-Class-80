1d55a91de11a16761029a02ccfd539e1cad267b4
Coding2Coding <76713830+Coding2Coding@users.noreply.github.com>
2021-05-30 09:49:35 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+var paragraph1 = [];
+var paragraph2 = [];
+
 function make1stParagraph() {
     var firstSentence = document.getElementById("1sentence").value;
     var secondSentence = document.getElementById("2sentence").value;
     var thirdSentence = document.getElementById("3sentence").value;
     var fourthSentence = document.getElementById("4sentence").value;
-    firstSentence.push("")
-    console.log(firstSentence+" "+secondSentence+" "+thirdSentence+" "+fourthSentence);
-    document.getElementById("1paragraph").innerHTML=firstSentence+" "+secondSentence+" "+thirdSentence+" "+fourthSentence;
+    paragraph1.push(firstSentence);
+    paragraph1.push(secondSentence);
+    paragraph1.push(thirdSentence);
+    paragraph1.push(fourthSentence);
+    var newParagraph1 = paragraph1.join(". ");
+    document.getElementById("1paragraph").innerHTML = newParagraph1;
+    console.log(paragraph1);
+    console.log(newParagraph1);
 }
 
 function make2ndParagraph() {
@@ -13,6 +21,12 @@ function make2ndParagraph() {
     var secondSentenceSecondParagraph = document.getElementById("2sentence2paragraph").value;
     var thirdSentenceSecondParagraph = document.getElementById("3sentence2paragraph").value;
     var fourthSentenceSecondParagraph = document.getElementById("4sentence2paragraph").value;
-    console.log(firstSentenceSecondParagraph+" "+secondSentenceSecondParagraph+" "+thirdSentenceSecondParagraph+" "+fourthSentenceSecondParagraph);
-    document.getElementById("2paragraph").innerHTML=firstSentenceSecondParagraph+" "+secondSentenceSecondParagraph+" "+thirdSentenceSecondParagraph+" "+fourthSentenceSecondParagraph;
+    paragraph2.push(firstSentenceSecondParagraph);
+    paragraph2.push(secondSentenceSecondParagraph);
+    paragraph2.push(thirdSentenceSecondParagraph);
+    paragraph2.push(fourthSentenceSecondParagraph);
+    var newParagraph2 = paragraph2.join(". ");
+    document.getElementById("2paragraph").innerHTML = newParagraph2;
+    console.log(paragraph2);
+    console.log(newParagraph2);
 }
